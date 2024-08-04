af113de2525f19c6bd4e1f697a2a0d83f0aa3a21
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-08-04 14:14:56 +0200

    Add TradeSpecifications

--- src/main/java/com/mycompany/p2pTradeSpringProject/persistence/repositories/TradeRepository.java
+++ src/main/java/com/mycompany/p2pTradeSpringProject/persistence/repositories/TradeRepository.java
@@ -3,10 +3,11 @@ package com.mycompany.p2pTradeSpringProject.persistence.repositories;
 import com.mycompany.p2pTradeSpringProject.persistence.entities.Currency;
 import com.mycompany.p2pTradeSpringProject.persistence.entities.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface TradeRepository extends JpaRepository<Trade, Integer> {
+public interface TradeRepository extends JpaRepository<Trade, Integer>, JpaSpecificationExecutor<Trade> {
     List<Trade> findByTradeCurrency(Currency currency);
     List<Trade> findByIsSeller(Boolean isSeller);
     List<Trade> findByTradeCurrencyAndIsSeller(Currency currency, Boolean isSeller);
---
+++ src/main/java/com/mycompany/p2pTradeSpringProject/persistence/repositories/specifications/TradeSpecifications.java
@@ -0,0 +1,36 @@
+package com.mycompany.p2pTradeSpringProject.persistence.repositories.specifications;
+
+import com.mycompany.p2pTradeSpringProject.persistence.entities.Status;
+import com.mycompany.p2pTradeSpringProject.persistence.entities.Trade;
+import com.mycompany.p2pTradeSpringProject.persistence.entities.Trade_;
+import org.springframework.data.jpa.domain.Specification;
+
+public class TradeSpecifications {
+
+    public static Specification<Trade> isSeller(Boolean isSeller) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.IS_SELLER), isSeller); //
+    }
+
+    public static Specification<Trade> tradeCurrencyId(Integer tradeCurrencyId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.TRADE_CURRENCY).get("id"), tradeCurrencyId);
+    }
+
+    public static Specification<Trade> exchangeCurrencyId(Integer exchangeCurrencyId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.EXCHANGE_CURRENCY).get("id"), exchangeCurrencyId);
+    }
+
+    public static Specification<Trade> isOpen() {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.STATUS), Status.open);
+    }
+
+    public static Specification<Trade> belongsToUser(Integer userId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.INITIATOR_USER).get("id"), userId);
+    }
+
+    public static Specification<Trade> repliedByUser(Integer userId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Trade_.RESPONDER_USER).get("id"), userId);
+    }
+
+
+
+}
