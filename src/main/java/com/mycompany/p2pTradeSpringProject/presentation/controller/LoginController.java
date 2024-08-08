247209bff10855afb482fb0fa7204f85b18272af
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-08-08 16:45:36 +0200

    Remove redundant login post method

--- src/main/java/com/mycompany/p2pTradeSpringProject/presentation/controller/LoginController.java
+++ src/main/java/com/mycompany/p2pTradeSpringProject/presentation/controller/LoginController.java
@@ -18,20 +18,4 @@ public class LoginController {
         return "login";
     }
 
-//    @PostMapping
-//    public String loginPost(LoginRequest loginRequest,
-//                            RedirectAttributes redirectAttributes,
-//                            HttpServletRequest request) {
-//
-//
-//        try {
-//            User authenticatedUser = authService.login(loginRequest);
-//            request.getSession().setAttribute("authenticatedUser", authenticatedUser);
-//            return "redirect:" + Urls.HOME; // Redirect to home page on successful login
-//
-//        } catch (BadCredentialsException e) {
-//            redirectAttributes.addFlashAttribute("error", "Invalid username or password");
-//            return "redirect:" + Urls.LOGIN; // Redirect back to login page on failure
-//        }
-//    }
 }
