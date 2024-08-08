53c7bd6d1d14462848dd37e9e11e65652c688d13
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-08-08 16:30:23 +0200

    Fix logout, remove redundant controller, change url patterns in config to constants

--- src/main/java/com/mycompany/p2pTradeSpringProject/security/WebSecurityConfig.java
+++ src/main/java/com/mycompany/p2pTradeSpringProject/security/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package com.mycompany.p2pTradeSpringProject.security;
 
+import com.mycompany.p2pTradeSpringProject.constants.Urls;
 import com.mycompany.p2pTradeSpringProject.persistence.daointerfaces.IDAOUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,23 +22,25 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/register", "/login").permitAll()
+                        .requestMatchers(Urls.ROOT, Urls.HOME, Urls.REGISTER, Urls.LOGIN).permitAll()
                         .anyRequest().authenticated()
                 )
 
                 .formLogin((form) -> form
-                        .loginPage("/login")
+                        .loginPage(Urls.LOGIN)
                         .failureHandler(customAuthenticationFailureHandler())
                         .permitAll()
-                )
-                .logout(Customizer.withDefaults());
+                ).logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher(Urls.LOGOUT, "GET"))
+                        .logoutSuccessUrl(Urls.LOGIN + "?logout")
+                );
 
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationProvider daoAuthenticationProvider(MyUserDetailService myUserDetailService, PasswordEncoder passwordEncoder) {
+    public AuthenticationProvider daoAuthenticationProvider(MyUserDetailService myUserDetailService, PasswordEncoder passwordEncoder) { // This bean is made because of need to set hideUserNotFoundExceptions to false
         DaoAuthenticationProvider impl = new DaoAuthenticationProvider();
         impl.setUserDetailsService(myUserDetailService);
         impl.setHideUserNotFoundExceptions(false);
