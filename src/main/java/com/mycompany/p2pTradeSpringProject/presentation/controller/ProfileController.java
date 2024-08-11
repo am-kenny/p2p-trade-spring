169a546bd7d2cb13c86df1b022f97a0ba356d157
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-08-11 21:51:57 +0200

    Add viewUsersBankAccounts method

--- src/main/java/com/mycompany/p2pTradeSpringProject/presentation/controller/ProfileController.java
+++ src/main/java/com/mycompany/p2pTradeSpringProject/presentation/controller/ProfileController.java
@@ -1,8 +1,10 @@
 package com.mycompany.p2pTradeSpringProject.presentation.controller;
 
 import com.mycompany.p2pTradeSpringProject.constant.Urls;
+import com.mycompany.p2pTradeSpringProject.domain.dto.bank.response.GetBankAccountsResponse;
 import com.mycompany.p2pTradeSpringProject.domain.entity.User;
 import com.mycompany.p2pTradeSpringProject.security.CustomUserDetails;
+import com.mycompany.p2pTradeSpringProject.service.BankAccountService;
 import com.mycompany.p2pTradeSpringProject.service.UserVerificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,16 +13,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping(Urls.PROFILE)
 @AllArgsConstructor
 public class ProfileController {
 
     private final UserVerificationService userVerificationService;
+    private final BankAccountService bankAccountService;
 
     @GetMapping
     public String profile(@AuthenticationPrincipal CustomUserDetails userDetails,
-                          Model model) { //TODO: Check Model vs ModelAndView
+                          Model model) {
 
         User user = userDetails.getUser();
 
@@ -28,5 +32,16 @@ public class ProfileController {
         model.addAttribute("isVerified", userVerificationService.isVerified(user.getId()));
         return "user/profile";
 
+    }
+
+    @GetMapping("/bank_accounts")
+    public String viewUsersBankAccounts(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                    Model model) {
+
+        User user = userDetails.getUser();
+        GetBankAccountsResponse response = bankAccountService.getUsersBankAccounts(user.getId());
+
+        model.addAttribute("bankAccounts", response.getBankAccounts());
+        return "user/bank_accounts";
     }
 }
